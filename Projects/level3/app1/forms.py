edb7682a4d5bf5e223215811083380881aac0b1a
Khosro HosseinAbadi <khosro.h2011@gmail.com>
2023-10-19 22:17:24 +0330

    Update content

--- Projects/level3/app1/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class CommentForm(forms.Form):
-    name = forms.CharField(max_length=200)
-    email = forms.EmailField()
-    text = forms.CharField(widget=forms.Textarea)
-
--- Projects/level3/app1/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.comment_form_view, name='comment_form_view'),
-]
